6313b9e14b753114be2f815190a3ca8b9be68131
Michael Tidwell <tidwell@zebedee.io>
2021-07-27 00:04:02 -0400

    eip remove provider

--- eip.tf
+++ eip.tf
@@ -5,11 +5,10 @@ resource "aws_eip" "i" {
   tags = merge(local.common_tags,tomap(
     { Name = "${var.alias}-ip" }
   ))
-  provider = aws.env
 }
+
 resource "aws_eip_association" "eip_assoc" {
   count = var.enable_external_ip == null ? 1 : 0
   instance_id   = aws_instance.i.id
   allocation_id = aws_eip.i[0].id
-  provider = aws.env
 }
